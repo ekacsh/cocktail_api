5aa1699957757559279487628a2c2e74622d225e
Pabolo Pires <pabolo18@gmail.com>
2020-08-04 15:01:50 -0300

    Troca axios por HttpModule

--- src/cocktail/cocktail.service.ts
+++ src/cocktail/cocktail.service.ts
@@ -1,28 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
 import { Cocktail } from './cocktail.entity';
-import axios from "axios";
 
 @Injectable()
 export class CocktailService {
+  constructor(private httpService: HttpService) { }
 
   async getRandom(): Promise<Cocktail> {
-    try { 
-      const response =  await axios.get("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-      const data = response.data["drinks"][0];
-      const cocktail =  Cocktail.fromJson(data);
-      return cocktail;
-    } catch (_) {
-      return new Cocktail();
-    }
+
+    return this.httpService.get("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+      .toPromise()
+      .then(res => res.data["drinks"][0])
+      .then(data => Cocktail.fromJson(data))
+      .catch(() => new Cocktail())
   }
 
   async getDrink(id: number): Promise<Cocktail> {
-    try { 
-      const response =  await axios.get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + id)
-      const cocktail =  Cocktail.fromJson(response.data["drinks"][0]);
-      return cocktail;
-    } catch (_) {
-      return new Cocktail();
-    }
+    return this.httpService.get("https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + id)
+      .toPromise()
+      .then(res => res.data["drinks"][0])
+      .then(data => Cocktail.fromJson(data))
+      .catch(() => new Cocktail())
   }
 }
